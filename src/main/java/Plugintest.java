99992f9e4fd9e86b192b7d53ed352e761880b000
Song-chanho <sch0622@postech.ac.kr>
2023-11-13 21:37:19 +0900

    Update : plugin.xml Plugintest class

--- src/main/java/Plugintest.java
+++ src/main/java/Plugintest.java
@@ -7,7 +7,7 @@ public class Plugintest extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
-        String menu = Messages.showInputDialog("What do you want to eat for lunch?", "Enter lunch menu", Messages.getQuestionIcon());
+        String menu = Messages.showInputDialog("This is the test message", "Enter message", Messages.getQuestionIcon());
         Messages.showMessageDialog(menu, "Enter", null);
     }
 }
