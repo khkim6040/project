9d82876a5ca62e8be6b02d6d1be915f32c00cd19
Song-chanho <sch0622@postech.ac.kr>
2023-11-18 20:00:58 +0900

    Add : BaseDetectAction, BaseDetectManager and LongParameterList Fix : Dependancy problem in plugin.xml (com.intellij.modules.java)

---
+++ src/main/java/BaseDetectManager.java
@@ -0,0 +1,37 @@
+
+/**
+ * Singleton Class with managing features of Multiple BaseDetectActions.
+ *
+ *
+ */
+public class BaseDetectManager {
+    private static BaseDetectManager manager = null;
+
+    /* Design Pattern: Singleton */
+    protected BaseDetectManager () { }
+
+    public static BaseDetectManager getInstance() {
+        if (manager == null)
+            manager = new BaseDetectManager();
+        return manager;
+    }
+
+    /**
+     * Method that fetches Refactoring Method name by ID.
+     *
+     * @param id Refactoring Techinque ID
+     * @return Corresponding Refactoring name (story name)
+     */
+    public BaseDetectAction getDetectActionByID (String id) {
+        switch (id) {
+            // Scope: Class
+            case "LPL":
+                return new LongParameterList();
+
+
+            default:
+                return null;
+        }
+    }
+
+}
