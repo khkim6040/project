433835f774bf6734f1a75ff0cb31956077dabc92
parkhyeonbeen <off1122@postech.ac.kr>
2023-11-20 22:01:26 +0900

    Add: SwitchStatement class
    #8 implemented basic detection class and code

--- src/main/java/detecting/BaseDetectManager.java
+++ src/main/java/detecting/BaseDetectManager.java
@@ -2,14 +2,13 @@ package detecting;
 
 /**
  * Singleton Class with managing features of Multiple BaseDetectActions.
- *
- *
  */
 public class BaseDetectManager {
     private static BaseDetectManager manager = null;
 
     /* Design Pattern: Singleton */
-    protected BaseDetectManager () { }
+    protected BaseDetectManager() {
+    }
 
     public static BaseDetectManager getInstance() {
         if (manager == null)
@@ -23,12 +22,13 @@ public class BaseDetectManager {
      * @param id Code Smell ID
      * @return Corresponding Detecting name (story name)
      */
-    public BaseDetectAction getDetectActionByID (String id) {
+    public BaseDetectAction getDetectActionByID(String id) {
         switch (id) {
             // Scope: Class
             case "LPL":
                 return new LongParameterList();
-
+            case "SS":
+                return new SwitchStatement();
 
             default:
                 return null;
