e0e72d1a786ecc7a6269ca33ebe53b925b93b12c
Jinmin Goh <eric9709@postech.ac.kr>
2023-11-25 00:29:55 +0900

    Update: Extract codescent.properties related function
    #14 Extracted functions into utils directory adopting singleton pattern.

---
+++ src/main/java/utils/UserConfigHandler.java
@@ -0,0 +1,47 @@
+package utils;
+
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModuleRootManager;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+public class UserConfigHandler {
+    private static UserConfigHandler handler = null;
+
+    private Properties prop;
+    private File configFile;
+    private FileInputStream fis;
+    private FileWriter writer;
+    public UserConfigHandler(Project project) throws IOException {
+        prop = new Properties();
+        String contentRoot = String.valueOf(ModuleRootManager.getInstance(ModuleManager.getInstance(project).getModules()[0]).getContentRoots()[0]);
+        contentRoot = contentRoot.replace("file://", "");
+        configFile = new File(contentRoot + "/codescent.properties");
+        fis = new FileInputStream(configFile);
+    }
+
+    public Properties getProp() {
+        return prop;
+    }
+
+    public FileWriter getWriter() throws IOException {
+        writer = new FileWriter(configFile);
+        return writer;
+    }
+
+    public FileInputStream getFis() {
+        return fis;
+    }
+
+    public static UserConfigHandler getHandler(Project project) throws IOException {
+        if (handler == null) {
+            handler = new UserConfigHandler(project);
+        }
+        return handler;
+    }
+}
