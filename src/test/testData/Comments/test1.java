f7dd3d5f6571f7b79ddb6a41dafe041af4761a6c
Song-chanho <sch0622@postech.ac.kr>
2023-11-29 22:24:56 +0900

    Add: add test code for comments W/ Jinyoung Kim
    #20

---
+++ src/test/testData/Comments/test1.java
@@ -0,0 +1,28 @@
+/**
+ * An example class with a smelly comments.
+ *
+ * @author Chanho Song
+ */
+public class TestDetectLargeClass {
+
+    public static void main(String[] args) {
+        /* This
+         * is
+         * 5
+         * line
+         * comments
+         */
+
+        /*
+         * TODO : This is Todo comment
+         */
+
+        /*
+         * Fix : This is Fix comment
+         */
+
+        int i;
+    }
+
+
+}
