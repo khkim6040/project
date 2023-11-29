441badcc7d0b3dd09edaa0cc5be0d603012783e2
Gwanho Kim <khkim6040@postech.ac.kr>
2023-11-29 22:36:05 +0900

    Update: Refactor doDetectSmellTest to abstract method
    Encapsulate doDetectSmellTest into abstract  SmellDetectorTest class.

--- src/test/java/DetectLargeClassTest.java
+++ src/test/java/DetectLargeClassTest.java
@@ -1,11 +1,5 @@
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.psi.PsiElement;
 import detecting.BaseDetectAction;
 import detecting.DetectLargeClass;
-import java.util.List;
 
 /**
  * Test for detecting large class'
@@ -20,27 +14,12 @@ public class DetectLargeClassTest extends SmellDetectorTest {
         return super.getBasePath() + "/DetectLargeClass";
     }
 
-    protected void doDetectSmellTest(int testNum, int expectedCount) {
-        myFixture.configureByFiles(getBasePath() + "/test" + testNum + ".java");
-        // Set up the action event with the necessary context
-        DataContext dataContext = DataManager.getInstance().getDataContext(myFixture.getEditor().getComponent());
-        AnActionEvent event = AnActionEvent.createFromDataContext(
-            String.valueOf(ActionManager.getInstance().getAction("")), null, dataContext);
-        // Run the action
-        BaseDetectAction action = new DetectLargeClass();
-        List<PsiElement> result = action.findSmells(event);
-        // Check the result
-        int detectedCount = result.size();
-        assertEquals(expectedCount, detectedCount);
+    @Override
+    protected BaseDetectAction getDetectAction() {
+        return new DetectLargeClass();
     }
 
     public void testDetectLargeClass1() {
         doDetectSmellTest(1, 1);
-        // Replace right number with the expected number of large classes in test1.java
     }
-
-//    public void testDetectLargeClass2() {
-//        doDetectSmellTest(2, 0);
-//        // Assume test2.java has 0 large classes
-//    }
 }
--- src/test/java/LongParameterListTest.java
+++ src/test/java/LongParameterListTest.java
@@ -1,11 +1,5 @@
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.psi.PsiElement;
 import detecting.BaseDetectAction;
 import detecting.LongParameterList;
-import java.util.List;
 
 /**
  * Test for detecting: 'Long parameter list'
@@ -19,25 +13,12 @@ public class LongParameterListTest extends SmellDetectorTest {
         return super.getBasePath() + "/LongParameterList";
     }
 
-    protected void doDetectSmellTest(int testNum, int expectedCount) {
-        myFixture.configureByFiles(getBasePath() + "/test" + testNum + ".java");
-        // Set up the action event with the necessary context
-        DataContext dataContext = DataManager.getInstance().getDataContext(myFixture.getEditor().getComponent());
-        AnActionEvent event = AnActionEvent.createFromDataContext(
-            String.valueOf(ActionManager.getInstance().getAction("")), null, dataContext);
-        // Run the action
-        BaseDetectAction action = new LongParameterList();
-        List<PsiElement> result = action.findSmells(event);
-        // Check the result
-        int detectedCount = result.size();
-        assertEquals(expectedCount, detectedCount);
+    @Override
+    protected BaseDetectAction getDetectAction() {
+        return new LongParameterList();
     }
 
     public void testLongParameterList1() {
         doDetectSmellTest(1, 1);
     }
-
-//    public void testLongParameterList2() {
-//        doDetectSmellTest(2, 0);
-//    }
 }
