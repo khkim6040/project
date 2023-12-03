a71c126d7802baaf7117628e32b828872bd834f8
Song-chanho <sch0622@postech.ac.kr>
2023-12-04 00:38:48 +0900

    Add: PoorName test case
    #29

---
+++ src/test/testData/PoorName/test4.java
@@ -0,0 +1,15 @@
+public class TestPoorName {
+    /**
+     * find poor names
+     *
+     * @author Chanho Song
+     */
+    public static void main(String[] args) {
+        TestIdentifyLongMethod example = new TestFindDuplicatedCode();
+        example.exampleMethodWithPoorName()
+    }
+
+    public void exampleMethodWithPoorName(){
+        int isgoodname = 1;
+    }
+}
